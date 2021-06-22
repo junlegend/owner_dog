b254f9e5ef085c5c72f79b3c218cb3d26734b2d9
choi myungjun <junlegend82@gmail.com>
2021-06-22 16:48:51 +0900

    GET 1st try

--- owner/views.py
+++ owner/views.py
@@ -10,7 +10,19 @@ class OwnerView(View):
         owners = Owner.objects.all()
         result = []
         for owner in owners:
-            result.append({"name": owner.name, "email": owner.email, "age": owner.age})
+            dogs = owner.dog_set.all()
+            dogs_list = []
+            for dog in dogs:
+                dogs_list.append({"1.dog_name": dog.name, "2.dog_age": dog.age})
+
+            result.append(
+                {
+                    "1.name": owner.name,
+                    "2.email": owner.email,
+                    "3.age": owner.age,
+                    "4.dog_list": dogs_list,
+                }
+            )
         return JsonResponse({"result": result}, status=200)
 
     def post(self, request):
@@ -25,13 +37,21 @@ class DogView(View):
         dogs = Dog.objects.all()
         result = []
         for dog in dogs:
-            result.append({"name": dog.name, "age": dog.age, "owner": dog.owner_id})
+            result.append(
+                {
+                    "1.name": dog.name,
+                    "2.age": dog.age,
+                    "3.owner_name": dog.owner.name,
+                }
+            )
         return JsonResponse({"result": result}, status=200)
 
     def post(self, request):
         data = json.loads(request.body)
         Dog.objects.create(
-            name=data["name"], age=data["age"], owner_id=data["owner_id"]
+            name=data["name"],
+            age=data["age"],
+            owner_id=data["owner_id"],
         )
 
         return JsonResponse({"massage": "SUCCESS"}, status=201)
